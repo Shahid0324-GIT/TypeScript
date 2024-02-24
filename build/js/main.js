78fb5c59060151a6023eb24d3f7437a61bd26ad1
Mohammed Shahid <ofcacc0324@gmail.com>
2024-02-24 13:50:17 +0530

    Practice-Day-1

---
+++ build/js/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let stringArray = ["Hey", "Hi", "Hello"];
+let numstrArray = ["Amigo", "Friend", 1];
+let mixedData = ["EVH", 1984, true];
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+let stringArray = ["Hey", "Hi", "Hello"];
+let numstrArray = ["Amigo", "Friend", 1];
+let mixedData = ["EVH", 1984, true];
